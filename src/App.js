d38a33f9ec73b7ef6d529803abcda6bd2f1286e6
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-02-22 19:11:32 +0100

    using .map to render JSX

--- src/App.js
+++ src/App.js
@@ -2,14 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon"
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"]
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"]
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"]
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"]
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <main>
-        <Title content="Some Simple Title" />
-        <Title content="Some Simple Text" />
-      </main>
+      <div className="row">
+        {all_pokemon.map(pokemon => (
+          <div className="col-md-6 col-lg-4">
+            <Pokemon
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          </div>
+        ))}
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

915bff404b1ef37bdbce5147ef45576162514ec8
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-02-22 11:50:05 +0100

    deployment ready

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React is amazing!!!!!
         </p>
         <a
           className="App-link"
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+           "Hello World" 
+           </a>
       </header>
     </div>
   );
